aef57d6b171925eba850b66a91a876a3e372c210
NathanBeals <bealsn@knights.ucf.edu>
2020-06-02 13:41:19 -0400

    still fighting with myself over the world sync, considering going back and making the character manager use a hashmap instead for fast lookups. (because the server will send a packet with a name and information to update)

--- Assets/Scripts/WorldServerSync.cs
+++ Assets/Scripts/WorldServerSync.cs
@@ -1,22 +1,21 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class WorldServerSync : MonoBehaviour
 {
-    static void AddCharacter()
+    // What will store the results from the server, TODO: likely to be replaced by the constructed Json stuff
+    struct CharacterDownlinkData
     {
+        string Name; // HACK: I am considering having this as an id instead of a string, but I'd have to hash it on this end, and I'm uncertain the best way to do that but I'm pretty sure it's needed, I don't know, it seems like it would be fine.
+        Vector3 Location; // Location in worldspace
+        Vector3 Rotation; // Direction of the body
+        //Vector3 Scale; // TODO: likely won't be used, used more for transform completeness
 
-    }
-
-    static void RemoveCharacter()
-    {
-
-    }
-
-    void SpawnCharacter()
-    {
-
+        Vector2 Input;
+        Vector3 LookRotation; // Camera rotation (which drives the movement)
     }
 
     void SyncTransformData()
