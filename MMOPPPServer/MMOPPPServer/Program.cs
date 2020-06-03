f1600b5b639d406e24e7feb84569414e8f7b865d
NathanBeals <bealsn@knights.ucf.edu>
2020-06-03 15:04:38 -0400

    I'm starting work on the server.

---
+++ MMOPPPServer/MMOPPPServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MMOPPPServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
