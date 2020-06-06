440a6b28119ce10a62e40755ac937a53e63e8f23
NathanBeals <bealsn@knights.ucf.edu>
2020-06-06 17:10:56 -0400

    playing around with ideas for what to do with this soon to be dead code, deleting it seems wrong, keeping it is also wrong, I want it around for sanity checks... for now I put it in a debug class and defined it out with debug.

--- MMOPPPServer/AIClient/Program.cs
+++ MMOPPPServer/AIClient/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿#define DEBUG
+
+using System;
 using System.Net;
 using System.Net.Sockets;
 using Google.Protobuf;
@@ -10,64 +12,43 @@ using System.Net.NetworkInformation;
 
 namespace AIClient
 {
+    class Debug
+    {
+        public static void PrintSizeCalc()
+        {
+#if (DEBUG)
+            PlayerInput testInput = new PlayerInput();
+            testInput.Id = new Identifier { Name = "Wilbur", Tags = "" };
+            testInput.MoveInput = new EntityInput
+            {
+                Strafe = false,
+                Sprint = false,
+                EulerRotation = new Vector3 { X = 0.0f, Y = 0.0f, Z = 0.0f },
+                DirectionInputs = new Vector3 { X = 0.0f, Y = 0.0f, Z = 0.0f }
+            };
+            testInput.SentTime = new Timestamp { Seconds = DateTime.Now.Second, Nanos = DateTime.Now.Millisecond / 1000000 };
+            Console.WriteLine("I am an AI2 {0}", testInput.CalculateSize());
+#endif
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            PlayerInput input = new PlayerInput
-            {
-                Id = new Identifier
-                {
-                    Name = "nate",
-                    Tags = "na"
-                },
-                MoveInput = new EntityInput
-                {
-                    DirectionInputs = new Vector3
-                    {
-                        X = 0.0f,
-                        Y = 0.0f,
-                        Z = 0.0f
-                    },
-                    EulerRotation = new Vector3
-                    {
-                        X = 0.0f,
-                        Y = 0.0f,
-                        Z = 0.0f
-                    },
-                    Sprint = false,
-                    Strafe = false
-                },
-                SentTime = new Timestamp() // TODO: this field is problematic, I want milisecond precision but it only takes whole seconds or nanoseconds... strang, might have to alter the message.
-                {
-                    Seconds = DateTime.Now.Second,
-                    Nanos = DateTime.Now.Millisecond / 1000000 //TODO: this converts mili to nanoseconds, this can't be right
-                }
-            };
+            Debug.PrintSizeCalc();
 
-            PlayerInput input2 = new PlayerInput();
-            input2.Id = new Identifier { Name = "Wilbur", Tags = ""};
-            input2.MoveInput = new EntityInput { 
-                Strafe = false, 
-                Sprint = false, 
-                EulerRotation = new Vector3 { X = 0.0f, Y = 0.0f, Z = 0.0f }, 
-                DirectionInputs = new Vector3 { X = 0.0f, Y = 0.0f, Z = 0.0f } };
-            input2.SentTime = new Timestamp { Seconds = DateTime.Now.Second, Nanos = DateTime.Now.Millisecond / 1000000 };
-
-            Console.WriteLine("I am an AI1 {0}", input.CalculateSize());
-            Console.WriteLine("I am an AI2 {0}", input2.CalculateSize());
-
-            input2.ToByteArray();
-
-            TCPPacket<PlayerInput> mypacket = new TCPPacket<PlayerInput> { m_Size = input2.CalculateSize(), m_Message = input2 };
+//            Packet<PlayerInput> mypacket = new Packet<PlayerInput> { m_Size = testInput.CalculateSize(), m_Message = testInput };
         }
     }
 
     //TODO: might be able to rename this just packet
-    public class TCPPacket<T> where T : Google.Protobuf.IMessage<T>
+    public class Packet<T> where T : Google.Protobuf.IMessage<T>
     {
         public Int32 m_Size;
         public T m_Message;
+
+        private Int32 m_Length;
 
         // TODO: there are more efficient ways to do this, I opted for simplicity
        Byte[] ToByteArray()
@@ -78,12 +59,15 @@ namespace AIClient
             byte[] c = new byte[a.Length + b.Length];
             Array.Copy(a, 0, c, 0, a.Length);
             Array.Copy(b, 0, c, a.Length, b.Length);
+
+            m_Length = c.Length;
+
             return c;
        }
 
         public void SendPacket(NetworkStream stream)
         {
-            stream.Write(ToByteArray(), 0, ToByteArray().Length);
+            stream.Write(ToByteArray(), 0, m_Length);
         }
     }
 }
