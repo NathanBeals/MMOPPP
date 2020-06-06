e708358a6e9914918dd2cd3ad729fcf8faced6b7
NathanBeals <bealsn@knights.ucf.edu>
2020-06-06 17:01:07 -0400

    checkin before cleanup

--- MMOPPPServer/AIClient/Program.cs
+++ MMOPPPServer/AIClient/Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Net;
 using System.Net.Sockets;
+using Google.Protobuf;
 using Google.Protobuf.Examples.AddressBook;
 using Google.Protobuf.MMOPPP.Messages;
 using Google.Protobuf.WellKnownTypes;
+using System.Linq;
+using System.Net.NetworkInformation;
 
 namespace AIClient
 {
@@ -53,11 +56,34 @@ namespace AIClient
 
             Console.WriteLine("I am an AI1 {0}", input.CalculateSize());
             Console.WriteLine("I am an AI2 {0}", input2.CalculateSize());
+
+            input2.ToByteArray();
+
+            TCPPacket<PlayerInput> mypacket = new TCPPacket<PlayerInput> { m_Size = input2.CalculateSize(), m_Message = input2 };
         }
     }
 
-    class Server
+    //TODO: might be able to rename this just packet
+    public class TCPPacket<T> where T : Google.Protobuf.IMessage<T>
     {
+        public Int32 m_Size;
+        public T m_Message;
 
+        // TODO: there are more efficient ways to do this, I opted for simplicity
+       Byte[] ToByteArray()
+       {
+            byte[] a = BitConverter.GetBytes(m_Size);
+            byte[] b = m_Message.ToByteArray();
+
+            byte[] c = new byte[a.Length + b.Length];
+            Array.Copy(a, 0, c, 0, a.Length);
+            Array.Copy(b, 0, c, a.Length, b.Length);
+            return c;
+       }
+
+        public void SendPacket(NetworkStream stream)
+        {
+            stream.Write(ToByteArray(), 0, ToByteArray().Length);
+        }
     }
 }
